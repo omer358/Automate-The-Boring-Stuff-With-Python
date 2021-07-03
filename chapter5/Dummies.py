03cc8c3fe50973d65ea3d5a9002127756b0afdaa
omer358 <omermaki358@gmail.com>
2021-07-03 12:19:19 +0200

    chapter 5

--- chapter5/Dummies.py
+++ chapter5/Dummies.py
@@ -39,6 +39,7 @@ allGuests = {'Alice': {'apples': 5, 'pretzels': 12},
              'Bob': {'ham sandwiches': 3, 'apples': 2},
              'Carol': {'cups': 3, 'apple pies': 1}}
 
+
 def total_brought(guests, item):
     num_brought = 0
     for k, v in guests.items():
